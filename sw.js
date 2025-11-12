1a68a840d4bccc6efd6fbb7238a33013bdf9a187
yzokumus <yzokumus@gmail.com>
2025-11-13 00:30:10 +0300

    PWA: start_url düzeltildi, service worker eklendi

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
+});
